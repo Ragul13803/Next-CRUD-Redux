49c83c12867fc1a58c8c82cf4724f2ea95dde5b3
Ragul <155870749+Ragul13803@users.noreply.github.com>
2025-04-07 21:28:31 +0530

    Update footer.tsx

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,6 +1,12 @@
-// components/Footer.jsx
 const Footer = () => (
-  <footer style={{ textAlign: 'center', padding: '10px', bgcolor: '#DDEB9D', color: '#143D60', }}>
+  <footer
+    style={{
+      textAlign: 'center',
+      padding: '10px',
+      backgroundColor: '#DDEB9D',
+      color: '#143D60',
+    }}
+  >
     &copy; 2025. All rights reserved.
   </footer>
 );
